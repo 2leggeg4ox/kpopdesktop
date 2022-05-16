84c30fdc27f4d5b0e524ba13f15db18d21a95263
Alex Haskins <alexanderwantshislibraryback@gmail.com>
2022-05-15 21:58:42 -0400

    v0.2 Reworking for prompts.

--- kpop.sh
+++ kpop.sh
@@ -1,17 +1,52 @@
 #!/bin/bash
 
-# >>> Welcome, I guess. I got tired of doing all this manually. So here I am.
+# This is a pretty simple shell script to swap GNOME out for KDE Plasma on Pop_OS!.
+# Obviously, this'll work on most, if not all, Debian-based distributions where the default DE is GNOME.
+# We're also going to ask the user if they'd like some additional packages and applications. 
+
 clear
-echo -e "Welcome to the K-Pop installer!\n\n"
-sleep 2s
-echo "Let's begin."
-sleep 1s
-clear
-echo "Checking for and installing any applicable system updates."
-sleep 1s
-sudo apt update && sudo apt upgrade
-clear
-echo "Installing repositories and configuring them."
+echo " _  __          ____               ___           _        _ _           "
+echo "| |/ /         |  _ \ ___  _ __   |_ _|_ __  ___| |_ __ _| | | ___ _ __ "
+echo "| ' /   _____  | |_) / _ \| '_ \   | || '_ \/ __| __/ _` | | |/ _ \ '__|"
+echo "| . \  |_____| |  __/ (_) | |_) |  | || | | \__ \ || (_| | | |  __/ |   "
+echo "|_|\_\         |_|   \___/| .__/  |___|_| |_|___/\__\__,_|_|_|\___|_|   "
+echo "                          |_|                                           "
+echo -e "\n \n"
+
+# Let's start by getting caught up to current.
+sudo apt update -y && sudo apt full-upgrade -y
+
+# Next up: we gotta actually install KDE Plasma. 
+# We're gonna explain the options. 
+# Then poll the user which they'd prefer.
+# Finally, we'll do the damn thing. Let's go!!
+
+echo -e "KDE has 3 levels of installation: \n"
+echo -e " 1. kde-plasma-desktop: Bare-minimum installation. Including basic utilities like a file manager, browser, and terminal emulator. \n"
+echo -e " 2. kde-standard: Everything in kde-plasma-desktop with a standard suit of system utilities. \n"
+echo -e " 3. kde-full: Everything in kde-standard with extra system utilities, games, and educational applications. \n"
+echo -e "\n"
+echo -e "\n"
+echo -e "When prompted, please select sddm. It is the preferred login manager for KDE. gdm3 is preferred for GNOME."
+
+#Poll
+read -p "Please select which you'd prefer. kde-standard (2) is recommended. [1/2/3/q]" varDistroLevel
+
+# Do it like Nike or whatever.
+if [ $varDistroLevel == 1]; then
+	sudo apt install kde-plasma-desktop
+elif [ $varDistroLevel == 2]; then
+	sudo apt install kde-standard
+elif [ $varDistroLevel == 3]; then
+	sudo apt install kde-full
+else
+	echo -e "Invalid option. Exiting. \n"
+	clear
+	exit 0
+fi
+
+
+
 sleep 1s
 sudo apt install -y curl apt-transport-https
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
@@ -50,9 +85,6 @@ echo "Cleanup time!"
 sleep 1s
 sudo apt autoremove --purge
 clear
-echo "And now, we're installing KDE/Plasma. Please select sddm when prompted."
-sleep 2s
-sudo apt install kde-full
 sudo apt install -y plasma-nm
 sudo apt purge -y dolphin kwrite konsole firefox
 echo "Removing Gnome"
